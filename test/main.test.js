b922ddcac143433c8043021dfc0ad34b21c68030
sonia.seuc <sonia.seuc@gmail.com>
2020-05-30 17:28:33 -0500

    2nd test pass: suma is a function

--- test/main.test.js
+++ test/main.test.js
@@ -4,4 +4,7 @@ describe('suma', () => {
   it('is a function', () => {
     expect(typeof main.suma).toBe('function');
   });
+  test('sumar 1 + 2 es igual a 3', () => {
+    expect(main.suma(1, 2)).toBe(3);
+  });
 });
