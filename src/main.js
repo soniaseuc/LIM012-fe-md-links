05c8cf9e7c2a782e24cfa6caf436e00aca34b8e6
sonia.seuc <sonia.seuc@gmail.com>
2020-06-08 11:51:03 -0500

    issue #5 create a path.isAbsolute function

--- src/main.js
+++ src/main.js
@@ -5,12 +5,59 @@ const suma = (a, b) => {
 };
 suma(8);
 
-const os = require('os');
 
-const cpu = os.cpus();
-console.log(cpu);
+// const os = require('os');
+// const cpu = os.cpus();
+// console.log(cpu);
 
 
 // module.exports = {
 //   suma,
 // };
+
+// file system
+const fs = require('fs');
+
+// fs.open('../test/readme.md', 'r', (err, fd) => {
+//   if (err) throw err;
+
+//   fs.close(fd, (err) => {
+//     if (err) throw err;
+//   });
+// });
+
+
+// // A class representing a directory stream.
+// async function print(path) {
+//   const dir = await fs.promises.opendir(path);
+//   for await (const dirent of dir) {
+//     console.log(dirent.name);
+//   }
+// }
+// print('./').catch(console.error);
+
+
+//  The fs.readFile() method is used to read files on your computer
+// fs.readFile();
+
+
+
+// trayectoria
+const path = require('path');
+
+path.isAbsolute('../test/readme.md');
+console.log(path.isAbsolute('/readme.md')); // true ('/test/readme.md') or ('/readme.md') | false ('../test/readme.md')
+
+// path.parse('C:\\path\\dir\\file.txt');
+// Returns:
+// { root: 'C:\\',
+//   dir: 'C:\\path\\dir',
+//   base: 'file.txt',
+//   ext: '.txt',
+//   name: 'file' }
+
+module.exports = {
+  path,
+  // suma,
+  // os,
+};
---
+++ test/readme.md
@@ -0,0 +1,1 @@
+https://www.google.com Inicio de GOOGLE
